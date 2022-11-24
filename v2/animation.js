9bd82799b0e6742ecfb3f2b26aa38464c86d7808
unknown <veselin.trifonov.tt@gmail.com>
2022-11-24 21:47:34 +0100

    Initial commit version 2 (Parcel and Popmotion import)

---
+++ v2/animation.js
@@ -0,0 +1,14 @@
+const popmotion = require("popmotion")
+
+const ball = document.querySelector(".ball")
+
+popmotion.animate({
+  from: "-100px",
+  to: "250px",
+  repeat: Infinity,
+  repeatType: "mirror",
+  type: "spring",
+  onUpdate(update) {
+    ball.style.top = update;
+  }
+})
